84d6b9780ab61cae893751554ecfaa8ee3d02e04
Arianne Boyd <boydarianne@yahoo.com>
2023-09-07 23:01:46 -0400

    adjusted variables in javascript and addded content to CSS

--- app.js
+++ app.js
@@ -1,72 +1,61 @@
-'use strict';
+"use strict";
 
-//constructor function that creates an object associated with each product
-function oddDuckProd(productName, filePath, timesShown){
-this.productName = productName;
-this.filePath = filePath;
-this.timesShown = 0;
+//constructor function that creates an object associated with each product (18)
+
+const imgVariable = {
+  photoArray: [
+    new product("bag", img / assets / bag.jpg),
+    new product("banana", img / assets / banana.jpg),
+    new product("bathroom", img / assets / bathroom.jpg),
+    new product("boots", img / assets / boots.jpg),
+    new product("breakfast", img / assets / breakfast.jpg),
+    new product("bubblegum", img / assets / bubblegum.jpg),
+    new product("chair", img / assets / chair.jpg),
+    new product("cthulhu", img / assets / cthulhu.jpg),
+    new product("dog-duck", img / assets / dog - duck.jpg),
+    new product("dragon", img / assets / dragon.jpg),
+    new product("pen", img / assets / pen.jpg),
+    new product("pet-sweep", img / assets / pet - sweep.jpg),
+    new product("scissors", img / assets / scissors.jpg),
+    new product("shark", img / assets / shark.jpg),
+    new product("sweep", img / assets / sweep.png),
+    new product("tauntaun", img / assets / tauntaun.jpg),
+    new product("unicorn", img / assets / unicorn.jpg),
+    new product("water-can", img / assets / water - can.jpg),
+    new product("wine-glass", img / assets / wine - glass.jpg),
+  ],
+};
+
+function product(productName, filePath, timesShown) {
+  this.productName = productName;
+  this.filePath = filePath;
+  this.timesShown = 0;
 }
 
+//algorithm that will randomly generate three unique product images from the images directory.
+for (let i=0; i < 3; i++){
+const number = Math.floor(Math.random()*imgVariable.length);
+}
 
-const product1 = new product();
-const product2 = new product();
-const product3 = new product();
-const product4 = new product();
-const product5 = new product();
-const product6 = new product();
-const product7 = new product();
-const product8 = new product();
-const product9 = new product();
-const product10 = new product();
-const product11 = new product();
-const product12 = new product();
-const product13 = new product();
-const product14 = new product();
-const product15 = new product();
-const product16 = new product();
-const product17 = new product();
-const product18 = new product();
-const product19 = new product();
+const productPhoto = [];
+const maxImages = 3;
 
+//constructor function to make image objects for images (this.name)
+function imageObj() {
+  this.imgKey = imgKey;
+}
+//function to select random images - similar to while loop
+//have the randomizer function trigger on a button click
 
-function displayRandomImg(){
-    const imgPaths = [
-        img/assets/bag.jpg,
-        img/assets/banana.jpg,
-        img/assets/bathroom.jpg,
-        img/assets/boots.jpg,
-        img/assets/breakfast.jpg,
-        img/assets/bubblegum.jpg,
-        img/assets/chair.jpg,
-        img/assets/cthulhu.jpg,
-        img/assets/dog-duck.jpg,
-        img/assets/dragon.jpg,
-        img/assets/pen.jpg,
-        img/assets/pet-sweep.jpg,
-        img/assets/scissors.jpg,
-        img/assets/shark.jpg,
-        img/assets/sweep.png,
-        img/assets/tauntaun.jpg,
-        img/assets/unicorn.jpg,
-        img/assets/water-can.jpg,
-        img/assets/wine-glass.jpg,
-    ];
-
-    const productPhoto = [];
-    const maxImages = 3;
-
-    while (productPhoto.length < maxImages) {
-        const randomImg = Math.floor(Math.random() * imagePaths.length);
-        if (productPhoto.includes(randomImg)) {
-          productPhoto.push(randomImg);
-        }
-    }
-};
-
+while (productPhoto.length < maxImages) {
+  const randomImg = Math.floor(Math.random() * imagePaths.length);
+  if (productPhoto.includes(randomImg)) {
+    productPhoto.push(randomImg);
+  }
+}
 for (let i = 0; i < maxImages; i++) {
-    const imgElm = document.getElementById(`image-container`);
-    imgElement.src = imagePaths[productPhoto[i]];
-};
+  const imgElm = document.getElementById(`image-container`);
+  imgElement.src = imagePaths[productPhoto[i]];
+}
 
 displayRandomImg();
-
